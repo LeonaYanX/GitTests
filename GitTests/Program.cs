ec37b0b03c3f94d6e62ac0747808e1a4485cd2e2
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-10-09 15:01:23 +0400

    Comm1

---
+++ GitTests/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GitTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<10; i++) 
+            {
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("GitTest");
+                Console.WriteLine("GitHub Hello!");  
+            }
+        }
+    }
+}
